d406d5f0ac102e8807c6af6c9c9ac59d8b617d70
Vito <agustinmediotti@gmail.com>
2021-06-27 05:37:48 -0300

    added react-route & components NavBar, Books

---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from 'reactstrap';
+ // Añadir
+const Navbar = (props) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen(!dropdownOpen);
+
+  return (
+    <div>
+      <Nav pills>
+        <NavItem>
+          <NavLink href="/firstPage" active>Libros</NavLink>
+        </NavItem>
+        <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+          <DropdownToggle nav caret>
+            Registro Prestados
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem header>Entradas</DropdownItem>
+            <DropdownItem>Añadir</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>Editar</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <NavItem>
+          <NavLink href="#">Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Another Link</NavLink>
+        </NavItem>
+      </Nav>
+    </div>
+  );
+}
+
+export default Navbar;
+
